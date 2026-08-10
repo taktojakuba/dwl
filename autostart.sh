9b604dcfe61a4ce67ebf3e2cd32beb2b6744fc67
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-08-10 19:00:54 +0200

    added clipboard startup

--- autostart.sh
+++ autostart.sh
@@ -5,4 +5,5 @@ set +e
 wlr-randr --output HDMI-A-1 --mode 1920x1080@119.878998Hz >/dev/null 2>&1 &
 noctalia >/dev/null 2>&1 &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 >/dev/null 2>&1 &
-~/.config/bashscripts/clipboardfix-wf.sh >/dev/null 2>&1 &
+wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
+wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
