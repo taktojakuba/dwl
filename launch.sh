37ff33cef0f0397578814ac1076f0c09aa0c6f73
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-08-12 23:26:30 +0200

    off load to gpu

--- launch.sh
+++ launch.sh
@@ -3,5 +3,6 @@ export XCURSOR_THEME=Future-dark-cursors
 export XCURSOR_SIZE=24
 export XDG_CURRENT_DESKTOP=dwl:wlroots
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
-export WLR_RENDERER=opengl
-exec /home/kuba/dwl/dwl -s /home/kuba/dwl/autostart.sh
+export WLR_RENDERER=gles2
+# gles2 vulkan
+exec prime-run /home/kuba/dwl/dwl -s /home/kuba/dwl/autostart.sh
