7e70373fb0aa346666e3c3e5c7396861f23b55d5
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-08-28 20:14:02 +0200

    fix

--- scripts/power.sh
+++ scripts/power.sh
@@ -4,8 +4,8 @@ choice=$(printf "Shutdown\nReboot\nLogout\nSuspend" | rofi -dmenu -p "Power")
 [ -z "$choice" ] && exit 1
 
 case "$choice" in
-	Shutdown) shutdown -p ;;
-	Reboot)   shutdown -r ;;
-	Logout)   loginctl terminate-user "$USER" ;;
+	Shutdown) shutdown ;;
+	Reboot)   reboot ;;
+	Logout)   loginctl terminate-user kuba ;;
 	Suspend)  loginctl suspend ;;
 esac
