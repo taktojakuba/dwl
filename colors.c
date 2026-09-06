bb072d477b81b556dfbc7e89bd2e6f825460e56e
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-06 22:09:20 +0200

    change type

--- colors.c
+++ colors.c
@@ -9,8 +9,8 @@
                         (hex & 0xFF) / 255.0f }
 
 /* colors */
-float rootcolor[]                   = COLOR(0x2e271fff);
-float bordercolor[]                 = COLOR(0x5d5346ff);
-float focuscolor[]                  = COLOR(0xb38f4fff);
-float urgentcolor[]                 = COLOR(0xa9634dff);
+float rootcolor[]                   = COLOR(0x141314ff);
+float bordercolor[]                 = COLOR(0x4a454aff);
+float focuscolor[]                  = COLOR(0xcfc3cdff);
+float urgentcolor[]                 = COLOR(0xffb4abff);
 const float fullscreen_bg[]         = {0.0f, 0.0f, 0.0f, 1.0f};
--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -8,4 +8,4 @@ wallpaper=$(find "$dir" -maxdepth 1 -type f \( -name '*.jpg' -o -name '*.jpeg' -
 
 pkill wbg 2>/dev/null
 wbg "$dir/$wallpaper" 2>/dev/null &
-matugen --prefer=value --type scheme-fidelity image "$dir/$wallpaper" &
+matugen --prefer=value image "$dir/$wallpaper" -t scheme-content &
