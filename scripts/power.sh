a02d2f6f6c01e993db5ecc1975965b02a978d7fc
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-21 21:01:36 +0200

    fix power.sh

--- scripts/power.sh
+++ scripts/power.sh
@@ -4,8 +4,8 @@ choice=$(printf "Shutdown\nReboot\nLogout\nSuspend" | rofi -dmenu -p "Power")
 [ -z "$choice" ] && exit 1
 
 case "$choice" in
-	Shutdown) sudo dinitctl poweroff ;;
-	Reboot)   sudo dinitctl reboot ;;
-	Logout)   pkill -u "$USER" ;;
-	Suspend)  sudo dinitctl suspend ;;
+	Shutdown) shutdown now ;;
+	Reboot)   reboot ;;
+	Logout)   loginctl terminate-user $USER ;;
+	Suspend)  loginctl suspend ;;
 esac
