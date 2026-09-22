e1a6693138097b2ab28559948434a6b636741d07
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-22 20:46:39 +0200

    change colors to monochrome

--- colors.c
+++ colors.c
@@ -9,8 +9,8 @@
                         (hex & 0xFF) / 255.0f }
 
 /* colors */
-float rootcolor[]                   = COLOR(0x141314ff);
-float bordercolor[]                 = COLOR(0x4a454aff);
-float focuscolor[]                  = COLOR(0xcfc3cdff);
-float urgentcolor[]                 = COLOR(0xffb4abff);
+float rootcolor[]                   = COLOR(0x000000ff);
+float bordercolor[]                 = COLOR(0x646464ff);
+float focuscolor[]                  = COLOR(0xffffffff);
+float urgentcolor[]                 = COLOR(0xffffffff);
 const float fullscreen_bg[]         = {0.0f, 0.0f, 0.0f, 1.0f};
