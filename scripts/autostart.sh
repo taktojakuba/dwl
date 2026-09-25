7a669ecb06098806b15814e43629d59f9c9db1a0
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-25 19:01:20 +0200

    fixup

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -4,8 +4,8 @@ set +e
 justbar &>/dev/null &
 #ironbar >/dev/null 2>&1 &
 mako >/dev/null 2>&1 &
-wbg ~/Pictures/Wallpapers/fav.jpg >/dev/null 2>&1 &
-
+wbg ~/Pictures/Wallpapers/monoforest.png >/dev/null 2>&1 &
+foot --server >/dev/null 2>&1 &
 wlr-randr --output HDMI-A-1 --mode 1920x1080@119.878998Hz >/dev/null 2>&1 &
 wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
 wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
